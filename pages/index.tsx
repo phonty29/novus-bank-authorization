634fd838f0c16a1ab9a6a388144cb566ee7f3be2
Amedov Bekmuhamet <96624483+phonty29@users.noreply.github.com>
2022-12-04 14:01:03 +0600

    build: mongoDB integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import PrimaryLayout from '../components/layout/primary/PrimaryLayout';
+import clientPromise from '../lib/mongodb';
 import { NextPageWithLayout } from './page';
 
-const Home: NextPageWithLayout = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    await clientPromise
+    return {
+      props: { isConnected: true },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: { isConnected: false },
+    };
+  }
+}
+
+const Home: NextPageWithLayout = ({
+  isConnected,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        {isConnected ? "You're connected to MongoDB" : "You are NOT connected to MongoDB"}
       </h1>
     </section>
   );
