47fb2c44e212aacfc0aeb787dc5ec779adfa8be2
Amedov Bekmuhamet <96624483+phonty29@users.noreply.github.com>
2022-12-13 15:46:40 +0600

    fix: delete not-existing props in footer

--- components/navigation/footer/Footer.tsx
+++ components/navigation/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export interface IFooter {}
 
-const Footer: React.FC<IFooter> = ({ sampleTextProp }) => {
+const Footer: React.FC<IFooter> = ({}) => {
   return <div className="footer"></div>;
 };
 
