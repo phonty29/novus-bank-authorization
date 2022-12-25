462c641c92fe7917d2508ff926ced1e51ebb6202
phonty29 <amedov.bekmuhamet@gmail.com>
2022-12-25 21:38:00 +0600

    ci: add stages array in ProgressBar.mocks.tsx

--- components/auth/progress-bar/ProgressBar.mocks.tsx
+++ components/auth/progress-bar/ProgressBar.mocks.tsx
@@ -3,6 +3,7 @@ import { IProgressBar } from './ProgressBar';
 
 const base: IProgressBar = {
   currentStage: SignUpStages.IDENTIFICATION,
+  stages: [],
 };
 
 export const mockProgressBarProps = {
