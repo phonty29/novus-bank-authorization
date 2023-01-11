9f1954ea6e5c76dd4c4cfecfc898ed20ee840646
phonty29 <amedov.bekmuhamet@gmail.com>
2023-01-11 09:41:34 +0600

    feat: responsive design for fonts and margins

--- components/auth/signup-fields/VerificationForm.tsx
+++ components/auth/signup-fields/VerificationForm.tsx
@@ -22,7 +22,7 @@ const VerificationForm: React.FC = () => {
 
   return (
     <>
-      <div className="input-field mb-7">
+      <div className="input-field">
         <label htmlFor="verification-method" className="label-text">
           Choose a verification method
           <span className="text-purple"> *</span>
@@ -45,13 +45,13 @@ const VerificationForm: React.FC = () => {
         </select>
       </div>
       <div
-        className={`green-btn verify-button mb-7`}
+        className={`green-btn verify-button`}
         onClick={sendVerification}
       >
         Send {buttonText}
       </div>
       {verificationState.verificationMethod === VerificationMethod.PHONE && (
-        <div className="input-field mb-7">
+        <div className="input-field">
           <label htmlFor="six-digit-code" className="label-text">
             Write the six-digit code that we sent to the phone number
             <span className="text-purple"> *</span>
