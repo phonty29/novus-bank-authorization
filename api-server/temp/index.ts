a776e275336d42e5ef27ffd1422704bd5fde8151
Amedov Bekmuhamet <96624483+phonty29@users.noreply.github.com>
2023-02-13 14:22:12 +0600

    feat: singleton for tempUserCollection

--- api-server/temp/index.ts
+++ api-server/temp/index.ts
@@ -1,42 +1,84 @@
-import Collections from "@utils/enums/Collections";
+import Collections from '@utils/enums/Collections';
 import Database from '@utils/helpers/db-singleton';
-import IUserData from "@utils/types/auth/IUserData";
+import IUserData from '@utils/types/auth/IUserData';
 import bcrypt from 'bcrypt';
-import { ObjectId } from "mongodb";
+import { Collection, ObjectId } from 'mongodb';
 
 class TempService {
+  singletonTempUserCollection = (() => {
+    let tempUserCollection: Collection<Document>;
+
+    let createTempUserCollection = async () => {
+      let collection = await Database.getCollection(Collections.TEMP_USERS);
+      collection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 60 });
+      return collection;
+    };
+
+    return {
+      getTempUserCollection: async () => {
+        if (!tempUserCollection)
+          tempUserCollection = await createTempUserCollection();
+        return tempUserCollection;
+      },
+    };
+  })();
+
   async getUserByUsername(username: string) {
-    let tempUserCollection = await Database.getCollection(Collections.TEMP_USERS);
-    tempUserCollection.createIndex( { "createdAt": 1 }, { expireAfterSeconds: 60 } );
-    let tempUser = await tempUserCollection.findOne({ "credentials.username": username });
+    let tempUserCollection = await Database.getCollection(
+      Collections.TEMP_USERS
+    );
+    tempUserCollection.createIndex(
+      { createdAt: 1 },
+      { expireAfterSeconds: 60 }
+    );
+    let tempUser = await tempUserCollection.findOne({
+      'credentials.username': username,
+    });
     return tempUser;
   }
 
   async getUserById(userId: string) {
-    let tempUserCollection = await Database.getCollection(Collections.TEMP_USERS);
-    tempUserCollection.createIndex( { "createdAt": 1 }, { expireAfterSeconds: 60 } );
-    let tempUser = await tempUserCollection.findOne({ "_id": new ObjectId(userId) });
+    let tempUserCollection = await Database.getCollection(
+      Collections.TEMP_USERS
+    );
+    tempUserCollection.createIndex(
+      { createdAt: 1 },
+      { expireAfterSeconds: 60 }
+    );
+    let tempUser = await tempUserCollection.findOne({
+      _id: new ObjectId(userId),
+    });
     return tempUser;
   }
 
   async addUser(userData: IUserData) {
     //каждый раз добавялет в коллекцию, не проверяет совпадают ли поля
-    const {credentials, personalInformation, accountInformation} = userData;
-    let tempUserCollection = await Database.getCollection(Collections.TEMP_USERS);
-    tempUserCollection.createIndex( { "createdAt": 1 }, { expireAfterSeconds: 60 } );
+    const { credentials, personalInformation, accountInformation } = userData;
+    let tempUserCollection = await Database.getCollection(
+      Collections.TEMP_USERS
+    );
+    tempUserCollection.createIndex(
+      { createdAt: 1 },
+      { expireAfterSeconds: 60 }
+    );
     let hashedPassword = await bcrypt.hash(credentials.password, 7);
-    let {insertedId} = await tempUserCollection.insertOne({
-      credentials: {...credentials, password: hashedPassword}, 
-      personalInformation, 
-      accountInformation
+    let { insertedId } = await tempUserCollection.insertOne({
+      credentials: { ...credentials, password: hashedPassword },
+      personalInformation,
+      accountInformation,
     });
     return insertedId.toString();
   }
 
   async deleteUser(userId: string) {
-    let tempUserCollection = await Database.getCollection(Collections.TEMP_USERS);
-    tempUserCollection.createIndex( { "createdAt": 1 }, { expireAfterSeconds: 60 } );
-    await tempUserCollection.deleteOne({ "_id": new ObjectId(userId) });
+    let tempUserCollection = await Database.getCollection(
+      Collections.TEMP_USERS
+    );
+    tempUserCollection.createIndex(
+      { createdAt: 1 },
+      { expireAfterSeconds: 60 }
+    );
+    await tempUserCollection.deleteOne({ _id: new ObjectId(userId) });
   }
 }
 
--- utils/helpers/db-singleton.ts
+++ utils/helpers/db-singleton.ts
@@ -2,21 +2,21 @@ import clientPromise from '@utils/mongodb';
 import { Db } from 'mongodb';
 
 class Database {
-  static Singleton = (() => {
+  private static Singleton = (() => {
     let database: Db;
 
     const createDB = async () => {
+      console.log('DATABASE CREATED ONCE AGAIN');
       let client = await clientPromise;
       return client.db(process.env.DB_NAME);
-    }
+    };
 
     return {
       getDB: async () => {
-        if (!database)
-          database = await createDB();
+        if (!database) database = await createDB();
         return database;
-      }
-    }
+      },
+    };
   })();
 
   static async getCollection(COLLECTION_NAME: string) {
