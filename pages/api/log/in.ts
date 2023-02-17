0aecf99dacfb7fb925d58c4530b9b4093633a66f
phonty29 <amedov.bekmuhamet@gmail.com>
2023-02-17 22:36:30 +0600

    fix: sign-in response message has type

--- pages/api/log/in.ts
+++ pages/api/log/in.ts
@@ -1,4 +1,5 @@
 import LogService from '@api-server/log';
+import AuthMessages from '@utils/enums/AuthMessages';
 import AuthError from '@utils/helpers/auth-error';
 import ICredentials from '@utils/types/auth/ICredentials';
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -10,7 +11,7 @@ interface ISignInRequestData extends NextApiRequest {
 export type ISignInResponseData = {
   refreshToken?: string;
   accessToken?: string;
-  message: string;
+  message: AuthMessages;
 };
 
 export default async function handler(
@@ -22,15 +23,15 @@ export default async function handler(
     case 'POST':
       try {
         let tokens = await LogService.in(req.body);
-        res.status(200).json({ ...tokens, message: "Пользователь успешно идентицифирован" });
+        res.status(200).json({ ...tokens, message: AuthMessages.LOG_IN_SUCCESS });
       } catch (error) {
         if (error instanceof AuthError) 
-            res.status(error.status).json({message: error.message})
-        res.status(500).json({ message: "Произошла ошибка сервера" });
+            res.status(error.status).json({message: error.message as AuthMessages})
+        res.status(500).json({ message: AuthMessages.AUTH_SERVER_ERROR });
       }
       break;
     default:
-      res.status(400).json({message: "Неверный запрос"});
+      res.status(400).json({message: AuthMessages.AUTH_BAD_REQUEST });
       break;
   }
 }
