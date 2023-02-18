8909bfed0c5bd55d4f58f87acd31c2fe294f8eba
phonty29 <amedov.bekmuhamet@gmail.com>
2023-02-19 00:08:33 +0600

    feat: simple footer

--- components/navigation/footer/Footer.tsx
+++ components/navigation/footer/Footer.tsx
@@ -1,7 +1,26 @@
-export interface IFooter extends React.ComponentPropsWithoutRef<'div'> {}
+import footerLinks from './footerLinks.json';
+
+interface IFooter extends React.ComponentPropsWithoutRef<'div'> {}
 
 const Footer: React.FC<IFooter> = () => {
-  return <div className="footer">Privacy policy</div>;
+  return (
+    <footer className="footer flex max-w-[1155px] items-center justify-between left-[55px] relative">
+      <small className="footer-copyright text-sm">
+        © All Rights Reserved. HCL Technologies
+      </small>
+      <ul className="footer-links flex text-xs">
+        {footerLinks.map((link, index) => {
+          return (
+            <li className="footer-link [&:not(:last-child):after]:content-['|'] after:mx-[10px]" key={index}>
+              <a href={link.href}>
+                {link.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
